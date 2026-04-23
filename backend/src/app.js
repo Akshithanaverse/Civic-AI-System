a8e417c1d6412ff03a11ef95f97ef87e6099535c
akshithanaverse <akshithanaverse@gmail.com>
2026-04-23 16:32:17 +0530

    This should now work

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,8 @@ import errorMiddleware from "./middleware/error.middleware.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/issues", issueRoutes);
